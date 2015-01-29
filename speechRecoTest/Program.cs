b4695a0f084ce99af123264a04aa0f070f0f41ee
oXis <oxis44@gmail.com>
2015-01-29 16:10:42 +0000

    add Command and regex

--- speechRecoTest/Program.cs
+++ speechRecoTest/Program.cs
@@ -33,57 +33,74 @@ namespace speechRecoTest
         static void Main(string[] args)
         {
 
-            Express.showMatch("Please, can you increase the speed", @"\bincrease\b");
+            //Express.showMatch("Please, can you increase the speed", @".*\bincrease.*\bspeed");
 
-            SpeechManager.PrintInstalledVoice();
-            List<VoiceInfo> infos = SpeechManager.GetInstalledVoice();
-
-            Console.WriteLine("\n\n");
+            //SpeechManager.PrintInstalledVoice();
+            //List<VoiceInfo> infos = SpeechManager.GetInstalledVoice();
 
             /*if (SpeechManager.SelectInstalledVoice("en-GB"))
             {
                 SpeechManager.Speak("I love you!");
-            }
-            if (SpeechManager.SelectInstalledVoice("en-US", 1))
-            {
-                SpeechManager.Speak("I love you!");
-            }
-            if (SpeechManager.SelectInstalledVoice("fr-FR"))
-            {
-                SpeechManager.Speak("Je t'aime !");
-            }
-            if (SpeechManager.SelectInstalledVoice("es-ES"))
-            {
-                SpeechManager.Speak("Te quiero !");
-            }
-            if (SpeechManager.SelectInstalledVoice("pl-PL"))
-            {
-                SpeechManager.Speak("Kocham cię !");
             }*/
 
             //Keyboard.ShortcutKeys(new Keys[] { Keys.P });
 
+            /*
             Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-FR");
             Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-FR");
             Console.WriteLine("Parle en Français, dire \"exit\" pour quiter");
             SpeechManager.Start(HandleSpeechRecognized);
+            */
 
-            ConsoleKeyInfo pressedKey = Console.ReadKey(true);
-            char keychar = pressedKey.KeyChar;
+            Command cmd = new Command(new List<string> {"caca", "speed"});
+            if (cmd.Matches("Please, can you increase the speed."))
+            {
+                Console.Write("Ok!\n");
+                Console.Write(cmd._Regex);
+            }
+            else
+            {
+                Console.Write("Not OK!\n");
+            }
+
+            cmd._Words = new List<string> { "increase", "speed" };
+            if (cmd.Matches("Please, can you increase the speed."))
+            {
+                Console.Write("Ok!\n");
+                Console.Write(cmd._Words);
+            }
+
+            /*
+            Command cmd = new Command();
+            cmd._play = @"C:\Users\Public\Music\Sample Music\Kalimba.mp3";
+
+            Command cmd2 = new Command();
+            cmd2._play = @"C:\Users\Public\Music\Sample Music\Sleep Away.mp3";
+
+            cmd.Play();
+            Thread.Sleep(1500);
+            cmd2.Play();
+            */
+            
+            /*
+            ConsoleKeyInfo pressedKey;
+            char keychar;
             do
             {
                 pressedKey = Console.ReadKey(true);
                 keychar = pressedKey.KeyChar;
                 Console.WriteLine("You pressed '{0}'", keychar);
-
+                if (keychar.Equals('s'))
+                {
+                    cmd.Stop();
+                    cmd2.Stop();
+                }
+            
             } while (!keychar.Equals('q'));
+            */
+            //SpeechManager.Stop();
+            
 
-            SpeechManager.Stop();
-
-            /*Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
-            Console.WriteLine("Speak in English, say \"exit\" to quit");
-            SpeechManager.Start();*/
         }
 
         static void HandleSpeechRecognized(object sender, SpeechRecognizedEventArgs e)
