021605b23880087def4de76358d262532382251d
oXis <oxis44@gmail.com>
2015-02-08 22:42:08 +0000

    add ActionExec and ActionKey

---
+++ speechRecoLib/ActionExec.cs
@@ -0,0 +1,42 @@
+﻿using speechRecoLib.ExecScript;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace speechRecoLib
+{
+    /// <summary>
+    /// Class that execute a program based on its name. Like notepad or chrome.
+    /// </summary>
+    class ActionExec : ActionManager
+    {
+        string _exec = null;
+
+        /// <summary>
+        /// Contructor.
+        /// </summary>
+        /// <param name="exec">Name of the script.</param>
+        public ActionExec(string exec)
+        {
+            _exec = exec;
+        }
+
+        public override bool Perform()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = _exec;
+            try
+            {
+                return p.Start();
+            }
+            catch
+            {
+                SpeechManager.Speak("Software " + _exec + "not recognised.");
+                return false;
+            }
+        }
+    }
+}
---
+++ speechRecoLib/ActionKey.cs
@@ -0,0 +1,54 @@
+﻿using InputManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace speechRecoLib
+{
+    class ActionKey : ActionManager
+    {
+        private Keys _key;
+        private Keys _keyMod = Keys.None;
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="key">Key to be pressed.</param>
+        public ActionKey(Keys key)
+        {
+            _key = key;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="key">Key to be pressed.</param>
+        /// <param name="keyMod">Modifier</param>
+        public ActionKey(Keys key, Keys keyMod)
+        {
+            _key = key;
+            _keyMod = keyMod;
+        }
+
+        /// <summary>
+        /// Press the _key (with _keyMod).
+        /// </summary>
+        /// <returns></returns>
+        public override bool Perform()
+        {
+            if (_keyMod == Keys.None)
+            {
+                Keyboard.KeyPress(_key);
+            } else
+            {
+                Keyboard.ShortcutKeys(new Keys[] { _keyMod, _key });
+            }
+            
+            return true;
+        }
+    }
+}
